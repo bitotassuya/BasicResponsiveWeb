f46933c727d5d823e43d7abd7d1685403182a283
bitotassuya <jareansong@gmail.com>
2022-04-20 22:06:00 +0700

    PGbarhide

--- Main.js
+++ Main.js
@@ -3,19 +3,22 @@ function myFunction() {
   alert("หน้าเว็บไม่พร้อมใช้งาน");
   if (i == 0) {
     i = 1;
+
     var elem = document.getElementById("myBar");
     var width = 1;
     var id = setInterval(frame, 10);
     function frame() {
       if (width >= 100) {
         clearInterval(id);
+
         i = 0;
       } else {
+        $("#myProgress").show();
+        $("#myBar").show();
         width++;
-        elem.style.width = width + "%";  
-        elem.innerHTML = width  + "%";
+        elem.style.width = width + "%";
+        elem.innerHTML = width + "%";
       }
     }
   }
 }
-
