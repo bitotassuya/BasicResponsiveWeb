6fa09329eb8ce7666c181e4a600a8a0d50b190ce
bitotassuya <jareansong@gmail.com>
2022-04-26 14:56:58 +0700

    Margin

--- Main.js
+++ Main.js
@@ -1,6 +1,7 @@
 var i = 0;
 function myFunction() {
   alert("หน้าเว็บไม่พร้อมใช้งาน");
+
   if (i == 0) {
     i = 1;
 
@@ -18,9 +19,17 @@ function myFunction() {
         width++;
         elem.style.width = width + "%";
         elem.innerHTML = width + "%";
+        location.href = "index.html";
+
       }
     }
+
   }
+
+}
+//hreflink
+function ReplaceLink() {
+  location.href = "index.html";
 }
 
 // Get the modal
@@ -32,3 +41,52 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+
+/* Login Authen*/
+
+var jwt = localStorage.getItem("jwt");
+if (jwt != null) {
+  window.location.href = "./index.html";
+}
+
+function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "https://www.mecallapi.com/api/login");
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhttp.send(
+    JSON.stringify({
+      username: username,
+      password: password,
+    })
+  );
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      const objects = JSON.parse(this.responseText);
+      console.log(objects);
+      if (objects["status"] == "ok") {
+        localStorage.setItem("jwt", objects["accessToken"]);
+        Swal.fire({
+          text: objects["message"],
+          icon: "success",
+          confirmButtonText: "OK",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.href = "./index.html";
+          }
+        });
+      } else {
+        Swal.fire({
+          text: objects["message"],
+          icon: "error",
+          confirmButtonText: "OK",
+        });
+      }
+    }
+  };
+  return false;
+}
+
+
